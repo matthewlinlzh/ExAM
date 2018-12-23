ad0290f7d4964ffd9d3cb6cbc59ce4111de2e070
matthew <apple@apples-MacBook-Pro.local>
2018-12-23 15:38:07 -0500

    update

--- pages/client/client.js
+++ pages/client/client.js
@@ -1,6 +1,13 @@
 const app = getApp()
 
 Page({
-  data:{},
+  data: {
+    product: [
+      { id: 1, image: "/pic_resource/interior.jpg", product_name: "Product Section 1" },
+      { id: 2, image: "/pic_resource/modification.png", product_name: "Product Section 2" },
+      { id: 3, image: "/pic_resource/fix.png", product_name: "Fix Car" },
+      { id: 4, image: "/pic_resource/change.png", product_name: "Change" }
+    ],
+  },
   
 })
--- pages/product/product.js
+++ pages/product/product.js
@@ -5,8 +5,10 @@ const app = getApp()
 Page({
   data:{
     product:[
-      {id:1, image: "/pic_resource/interior.jpg", product_name: "Product Section"},
-      {id:2, image: "/pic_resource/modification.png", product_name: "Product Section"}
+      {id:1, image: "/pic_resource/interior.jpg", product_name: "Product Section 1"},
+      {id:2, image: "/pic_resource/modification.png", product_name: "Product Section 2"},
+      {id: 3, image: "/pic_resource/fix.png", product_name: "Fix Car"},
+      { id: 4, image: "/pic_resource/change.png", product_name: "Change" }
     ],
   },
   // 跳转页面并携带该页面的信息
