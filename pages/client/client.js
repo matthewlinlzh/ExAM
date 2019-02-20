3e48427f2d2423f0a72487c759ed68a057036d7a
matthew <apple@apples-MacBook-Pro.local>
2019-02-19 21:46:55 -0500

    update client vender

--- pages/client/client.js
+++ pages/client/client.js
@@ -2,12 +2,21 @@ const app = getApp()
 
 Page({
   data: {
-    product: [
-      { id: 1, image: "/pic_resource/interior.jpg", product_name: "Product Section 1" },
-      { id: 2, image: "/pic_resource/modification.png", product_name: "Product Section 2" },
-      { id: 3, image: "/pic_resource/fix.png", product_name: "Fix Car" },
-      { id: 4, image: "/pic_resource/change.png", product_name: "Change" }
+    vender: [
+      { id: 1, vender_pic: "/pic_resource/interior.jpg", vender_name: "Product Section 1", vender_product: ["/pic_resource/interior.jpg", "/pic_resource/modification.png", "/pic_resource/fix.png"] },
+      { id: 2, vender_pic: "/pic_resource/modification.png", vender_name: "Product Section 2", vender_product: ["/pic_resource/modification.png"] },
+      { id: 3, vender_pic: "/pic_resource/fix.png", vender_name: "Fix Car", vender_product: ["/pic_resource/interior.jpg", "/pic_resource/modification.png", "/pic_resource/fix.png"] },
     ],
+    indicator_dots: true,
+    autoplay: true,
+    circular: true,
+    autoplay: true,
+    interval: 2500,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
   },
-  
+   onLoad(){
+     
+   }
 })
--- pages/product/product.js
+++ pages/product/product.js
@@ -7,8 +7,7 @@ Page({
     product:[
       {id:1, image: "/pic_resource/interior.jpg", product_name: "Product Section 1"},
       {id:2, image: "/pic_resource/modification.png", product_name: "Product Section 2"},
-      {id: 3, image: "/pic_resource/fix.png", product_name: "Fix Car"},
-      { id: 4, image: "/pic_resource/change.png", product_name: "Change" }
+      {id: 3, image: "/pic_resource/fix.png", product_name: "Fix Car"}
     ],
   },
   // 跳转页面并携带该页面的信息
